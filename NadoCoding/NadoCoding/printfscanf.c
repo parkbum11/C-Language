1f96f3588a7caf140e80f33aecb1ded64eff19a3
parkbum <parkbum11@naver.com>
2021-02-22 22:59:10 +0900

    first commit

---
+++ NadoCoding/NadoCoding/printfscanf.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*int age = 12;
+	printf("%d\n", age);
+
+	age = 13;
+	printf("%d\n", age);*/
+
+	/*float f = 45.5;
+	printf("%.2f\n", f);
+
+	double d = 4.428;
+	printf("%.3lf\n", d);*/
+
+	/*const int YEAR = 2000;
+	printf("태어난 연도 : %d\n", YEAR);
+
+	int add = 3 + 7;
+	printf("%d\n", add);
+	printf("%d + %d = %d\n", 3, 7, 3 + 7);*/
+
+	// scanf
+	// 키보드 입력을 받아서 저장
+	/*int input;
+	printf("값을 입력하세요 : ");
+	scanf_s("%d", &input);
+	printf("입력값은 : %d\n", input);
+
+	int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("%d %d %d\n", one, two, three);*/
+
+	/*char c = 'A';
+	printf("%c\n", c);*/
+
+	/*char str[256];
+	scanf_s("%s", str, sizeof(str));
+	printf("%s\n", str);*/
+
+
+
+	return 0;
+}
